891a450059becd3a249a4b17344be32a513e877c
jorgev259 <jorgev259@outlook.es>
2021-09-05 16:22:59 -0600

    [saucenao] Fix in-memory channel enabling

--- saucenao/commands.js
+++ saucenao/commands.js
@@ -3,11 +3,11 @@ module.exports = {
     desc: 'Enables saucebot on the current channel',
     async execute ({ sequelize, config }, { message }) {
       const guild = message.guild.id
-      const channel = message.guild.id
+      const channel = message.channel.id
 
       if (config[guild].saucenao.has(channel)) return message.channel.send('Saucenao already enabled on this channel')
 
-      await sequelize.models.saucenao.findOrCreate({ where: { guild: message.guild.id, channel: message.channel.id } })
+      await sequelize.models.saucenao.findOrCreate({ where: { guild, channel } })
       config[guild].saucenao.add(channel)
       message.channel.send('Saucenao enabled on this channel')
     }
@@ -16,11 +16,11 @@ module.exports = {
     desc: 'Disables saucebot on the current channel',
     async execute ({ sequelize, config }, { message }) {
       const guild = message.guild.id
-      const channel = message.guild.id
+      const channel = message.channel.id
 
       if (!config[guild].saucenao.has(channel)) return message.channel.send('Saucenao not enabled on this channel')
 
-      await sequelize.models.saucenao.destroy({ where: { guild: message.guild.id, channel: message.channel.id } })
+      await sequelize.models.saucenao.destroy({ where: { guild, channel } })
       config[guild].saucenao.delete(channel)
       message.channel.send('Saucenao disabled from this channel')
     }

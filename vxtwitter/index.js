c9efc216b36d3f9235cc601efcf64f50d4ca8d59
jorgev259 <jorgev259@outlook.es>
2024-01-13 18:03:04 -0600

    [vxtwitter] Minor refactor

--- vxtwitter/index.js
+++ vxtwitter/index.js
@@ -1,33 +1,36 @@
-const getUrls = require('get-urls')
+import getUrls from 'get-urls'
+
 const searchStrings = ['https://twitter.com', 'https://x.com', 'https://t.co']
 
-module.exports = {
-  name: 'fxtwitter',
+const vxtwitter = {
+  name: 'vxtwitter',
   events: {
     messageCreate ({ client }, msg) {
       if (msg.author.id === client.user.id || msg.author.bot) return
 
       const urls = Array.from(getUrls(msg.content))
-        .filter(url => searchStrings.some(search => url.includes(search)))      
-      
-        if (urls.length > 0) {
-          let content  = msg.content
-          for (search of searchStrings){
-            content = content.replaceAll(search, 'https://vxtwitter.com')
-          }
+        .filter(url => searchStrings.some(search => url.includes(search)))
 
-          const users = Array.from(msg.mentions.users.keys())
-            .filter(snowflake => snowflake !== msg.author.id)
-
-          msg.channel.send({
-            content: `${msg.author}: ${content}`,
-            allowedMentions: {
-              repliedUser: false,
-              users
-            }
-          })
-            .then(() => msg.delete())
+      if (urls.length > 0) {
+        let content = msg.content
+        for (const search of searchStrings) {
+          content = content.replaceAll(search, 'https://vxtwitter.com')
         }
+
+        const users = Array.from(msg.mentions.users.keys())
+          .filter(snowflake => snowflake !== msg.author.id)
+
+        msg.channel.send({
+          content: `${msg.author}: ${content}`,
+          allowedMentions: {
+            repliedUser: false,
+            users
+          }
+        })
+          .then(() => msg.delete())
+      }
     }
   }
 }
+
+export default vxtwitter
